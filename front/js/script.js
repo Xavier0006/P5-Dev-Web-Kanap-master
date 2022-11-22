0926e4a05059ac0ca4bffb89ba10f7a1116d7146
Xabi <108257391+Xavier0006@users.noreply.github.com>
2022-11-22 16:36:12 +0100

    commit tuesday

--- front/js/script.js
+++ front/js/script.js
@@ -1,2 +1,37 @@
-/*  Fetching data from the API  */
+/*  Fetching datas from the API  */
+
+document.addEventListener("DOMContentLoader", () => {
+
+    getItems()
+
+});
+
+/* Get all Items from the API */
+
+async function getItems() {
+
+    try {
+        const response = await fetch("http://localhost:3000/api/products");
+        console.log(response.status);
+        if (!response.ok) {
+            throw new Error("Error HTTP");
+        }
+
+        const data = await response.json(); // Formater la réponse en json 
+
+        const itemList = document.getElementById("items");
+
+        for (const item of data) {
+            addNewItem(item, itemList);
+        }
+
+    }
+    catch (e) {
+        console.error(e);
+    }
+
+
+}
+
+
 
