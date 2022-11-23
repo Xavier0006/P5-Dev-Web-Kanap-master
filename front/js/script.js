7224639f7e14c6f7c362ad070f13ced6bc6fccfd
Xabi <108257391+Xavier0006@users.noreply.github.com>
2022-11-23 14:21:57 +0100

    commit wed

--- front/js/script.js
+++ front/js/script.js
@@ -1,54 +1,21 @@
-/*  Fetching datas from the API  */
+/* Appel à l'API */
 
-document.addEventListener("DOMContentLoader", () => {
-
-    getItems()
-
-});
-
-/* Get all Items from the API 
-
-async function getItems() {
-
-    try {
-        const response = await fetch("http://localhost:3000/api/products");
-        console.log(response.status);
-        if (!response.ok) {
-            throw new Error("Erreur merci de vérifier votre connexion");
-        }
-
-        const data = await response.json(); // Formater la réponse en json 
-
-        const itemList = document.getElementById("items");
-
-        for (const item of data) {
-            console.log(item);
-            addNewItem(item, itemList);
-        }
-
-    }
-    catch (e) {
-        console.error(e);
-    }
-
-
-}
-
-/* Add nouveau item au DOM */
-
-/* 
-{object} item 
- {element} Parent
-
-*/
-/*
-const addNewItem = (item, Parent) => {
-
-};
-
-*/
 
 let url = "http://localhost:3000/api/products";
 
-fetch(url).then(response => response.json().then(data => console.log(data))
-);
+// Fetch récupère les données depuis l'URL de l'API
+
+fetch(url)
+    .then(response => response.json())
+    
+// La premiere promesse then va récuperer la réponse et la transforme en json 
+// Ensuite deuxieme promesse qui va afficher les données conetenue dans ma f
+    .then(data =>  { voirProduit(data);
+
+    })
+
+    // Ajout d'un message en cas d'erreur
+
+    .catch(e => { alert ("Fonctionnement impossible")
+
+    } );
