7af8d0cf12ee0e86d46e94ecda7838b6e27520b2
Xabi <108257391+Xavier0006@users.noreply.github.com>
2022-11-29 17:37:42 +0100

    commit tuesday

--- front/js/product.js
+++ front/js/product.js
@@ -6,9 +6,25 @@ const displayPrice = getElementById('price');
 const productDescription = getElementById("description");
 const displayColors = getElementByID("colors");
 
-// Faire appel à l'API + modifier URL ? ( comment faire.. )
+let urlImg = "";
+let urlAlt = "";
 
-// load productlist appel l'API vide le tableau en global 
-// refreshlist purge la liste 
-// fonction evennementiel sert à refresh le detail produit
+
+// Faire appel à l'API + création d'un nouvel ID afin de récuperer les données
+
+const params = new URLSearchParams(window.location.search);
+
+const id = params.get("id");
+
+const url = `http://localhost:3000/api/products${id}`;
+
+getProducts = () => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            itemImg.innerHtml = `<img src="${data.urlImg}" alt="${data.urlAlt}">`
+            urlImg = data.urlImg;
+            urlAlt = data.urlAlt;
+        })
+}
 
