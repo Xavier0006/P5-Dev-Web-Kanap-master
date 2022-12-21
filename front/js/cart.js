5c103a00b1d3191e433f3f373c64cdc2edfe8bff
Xabi <108257391+Xavier0006@users.noreply.github.com>
2022-12-21 18:01:58 +0100

    commit thursday

---
+++ front/js/cart.js
@@ -0,0 +1,20 @@
+// Récupérer les produits dans le local storage 
+
+const produits = [];
+const addCartToLocalStorage = JSON.parse(localStorage.getItem("cart"));
+
+// Les afficher
+
+let cartAndFormContainer = getElementById("cartAndFormContainer")
+
+// si panier vide afficher message 
+
+// Modifier
+
+// Actualiser 
+
+// Supprimer
+
+// Afficher et calculer le total des articles dans le panier final 
+
+// !!!! ne pas stocker le prix des articles en local car non sécurisés !!!!
