03962b3336bd4362804e687886c9df2df3b0e2e4
Xabi <108257391+Xavier0006@users.noreply.github.com>
2023-01-21 15:20:15 +0100

    commit saturday

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,1 +1,9 @@
-// confirmation à faire 
+// Faire un localstorage clear car toutes les saisies devraient être conservés sur cette page
+
+function payment() {
+    const orderId = document.getElementById("orderId");
+    orderId.innerHTML = localStorage.getItem("orderId");
+    localStorage.clear();
+};
+
+// remove item localstorage au lieu d'un clear 
